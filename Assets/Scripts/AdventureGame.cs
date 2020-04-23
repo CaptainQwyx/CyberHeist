f13572bf48f1126e6b0f958b7bdbcad19cc1120c
Jeff Anderson <captainqwyx@gmail.com>
2020-04-23 11:10:30 -0500

    finished project for Cyber Heist

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent = null;
+    [SerializeField] GameState initialState = null;
+    [SerializeField] GameState finalState = null;
+    [SerializeField] int numReplacements = 0;
+
+    GameState state = null;
+
+    string[,] replacements = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        replacements = new string[numReplacements, 2];
+
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void InitGame()
+    {
+        state = initialState;
+        textComponent.text = state.GetStateStory(replacements);
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        for (int index = 0; index < nextStates.Length; index++)
+        {
+            if (Input.GetKeyDown(KeyCode.A + index))
+            {
+                int replaceIndex = state.GetReplaceIndex();
+               
+                if(replaceIndex >= 0)
+                {
+                    replacements[replaceIndex, 0] = state.GetKeyText();
+                    replacements[replaceIndex, 1] = state.GetResultPhrase(index);
+                }
+                state = nextStates[index];
+                textComponent.text = state.GetStateStory(replacements);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            state = finalState;
+            textComponent.text = state.GetStateStory(replacements);
+        }
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "GameState")]
+public class GameState : ScriptableObject
+{
+
+    [TextArea(10, 14)] [SerializeField] string storyText = "";
+    [SerializeField] int replaceIndex = -1;
+    [SerializeField] string keyText = "";
+    [SerializeField] string[] resultPhrases = null;
+
+    [SerializeField] GameState[] nextStates = null;
+
+    public string GetStateStory(string[,] replacements)
+    {
+        string result = storyText;
+
+//        Debug.Log("replacements.length = " + replacements.GetLength(0));
+
+        for (int index = 0; index < replacements.GetLength(0); index++)
+        {
+//            Debug.Log("replacements[" + index + ", 0] = " + replacements[index, 0]);
+            if (replacements[index, 0] != null && replacements[index, 0].Length > 0)
+            {
+//                Debug.Log("replacements[" + index + ", 1] = " + replacements[index, 1]);
+                result = result.Replace(replacements[index, 0], replacements[index, 1]);
+            }
+        }
+
+        return result;
+    }
+
+    public GameState[] GetNextStates()
+    {
+        return nextStates;
+    }
+    
+    public int GetReplaceIndex()
+    {
+        return replaceIndex;
+    }
+    public string GetKeyText()
+    {
+        return keyText;
+    }
+    public string GetResultPhrase(int index)
+    {
+        return resultPhrases[index];
+    }
+}
